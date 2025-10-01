4647d4363cbed404afb4504420df3b825ba96608
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2025-10-01 23:54:52 +0700

    feat: add siperahu boiler plate page

---
+++ src/app/(landing)/sipperahu/page.tsx
@@ -0,0 +1,24 @@
+import { headers } from "next/headers";
+import { ProfileKecamatanService } from "@/lib/prisma-service/profilekecamatanService";
+import type { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Siperahu - Sistem Penilaian Kinerja Paruh Waktu",
+    description:
+      "Dengan Siperahu, Kecamatan Anda dapat menilai kinerja paruh waktu secara efektif.",
+  };
+}
+
+export default async function SipperahuPage() {
+  const headersList = headers();
+  const host = headersList.get("host") || "";
+  const subdomain = host.split(".")[0];
+  const kecamatan =
+    await ProfileKecamatanService.getKecamatanProfileBySubdomain(subdomain);
+  return (
+    <div>
+      <div>{/*  */}</div>
+    </div>
+  );
+}
